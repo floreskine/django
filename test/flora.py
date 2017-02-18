149cf7f5f10f8e0bfe461eb826dea2978b312ddc
floreskine <flora@MacBookdiFlora.station>
2017-02-18 17:58:48 +0100

    blog django girls

---
+++ test/flora.py
@@ -0,0 +1,19 @@
+print("un brivido di emozione")
+if 3 > 2:
+    print("3 è sempre maggiore di 2")
+else:
+    print("3 non è maggiore di 2")
+def django():
+    print("eccomi")
+django()
+def djangobis():
+    print("ci sto provando")
+djangobis()
+djangobis()
+
+def ciao(citta):
+    print("vengo da " + citta + "!")
+
+citta_elenco = ["Napoli", "Roma", "Ginevra", "Milano"]
+for citta in citta_elenco:
+    ciao(citta)
